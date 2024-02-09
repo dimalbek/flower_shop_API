c7c5b52437fbf5b7478037432639c5cdd8c52daa
dimalbek <dimalbek@gmail.com>
2024-02-09 22:27:09 +0600

    users repo basics implemented

--- app/users_repository.py
+++ app/users_repository.py
@@ -15,5 +15,29 @@ class UsersRepository:
         self.users = []
 
     # необходимые методы сюда
+    def get_all(self):
+        return self.users
+
+    def get_one(self, id):
+        for user in self.users:
+            if id == user.id:
+                return user
+        return None
+
+    def save(self, user: User):
+        user.id = len(self.users) + 1
+        self.users.append(user)
+        return user
+
+    def update(self, id: int, input: User):
+        for i, user in enumerate(self.users):
+            if user.id == id:
+                input.id = id
+                self.users[i] = input
+
+    def delete(self, id: int,):
+        for i, user in enumerate(self.users):
+            if user.id == id:
+                del self.users[i]
 
     # конец решения
