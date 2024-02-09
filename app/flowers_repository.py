2ff0f94129765d9b897acbf4be538ba92a590018
dimalbek <dimalbek@gmail.com>
2024-02-09 22:24:27 +0600

    flowers repo basics implemented

--- app/flowers_repository.py
+++ app/flowers_repository.py
@@ -16,5 +16,29 @@ class FlowersRepository:
         self.flowers = []
 
     # необходимые методы сюда
+    def get_all(self):
+        return self.flowers
+
+    def get_one(self, id):
+        for flower in self.flowers:
+            if id == flower.id:
+                return flower
+        return None
+
+    def save(self, flower: Flower):
+        flower.id = len(self.flowers) + 1
+        self.flowers.append(flower)
+        return flower
+
+    def update(self, id: int, input: Flower):
+        for i, flower in enumerate(self.flowers):
+            if flower.id == id:
+                input.id = id
+                self.flowers[i] = input
+
+    def delete(self, id: int,):
+        for i, flower in enumerate(self.flowers):
+            if flower.id == id:
+                del self.flowers[i]
 
     # конец решения
